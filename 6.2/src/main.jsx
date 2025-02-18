9c5628098f808036a96a73dae57ff78f4f49f020
kon1aev7 <konyaevd1995@gmail.com>
2025-02-18 12:05:03 +0200

    new

---
+++ 6.2/src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import "modern-normalize";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+createRoot(document.getElementById("root")).render(
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <Toaster position="top-right" toastOptions={{ duration: 1000 }} />
+  </>
+);
